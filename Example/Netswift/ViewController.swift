578e65d4422d75a91e9617f225c26fdc7406f03d
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2019-12-08 17:23:10 +0100

    🔥 Remove placeholder code

--- Example/Netswift/ViewController.swift
+++ Example/Netswift/ViewController.swift
@@ -25,11 +25,5 @@ class ViewController: UIViewController {
             }
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
