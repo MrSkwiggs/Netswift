f19598194290cc53b57be0011c4c67dbf23a0d9f
Dorian Grolaux <dorian@skwiggs.dev>
2020-01-16 15:28:46 +0100

    🐛 Fix compilation issues

--- Tests/NetswiftTests/NetswiftTests.swift
+++ Tests/NetswiftTests/NetswiftTests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import Netswift
 
 final class NetswiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Netswift().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    static var allTests: [(String, (NetswiftTests) -> () -> ())] = []
 }
