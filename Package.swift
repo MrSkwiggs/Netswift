f5e7975a522fa76d7d0111b60437967d9188f1d6
Dorian Grolaux <dorian@skwiggs.dev>
2020-01-17 10:37:29 +0100

    🔧 Remove platforms specifier

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Netswift",
-    platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v12), .macOS(.v10_13)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
