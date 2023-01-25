af613c1d0fc02c7cda319e42be741b5dfb928e69
Dorian <6209874+MrSkwiggs@users.noreply.github.com>
2023-01-25 14:01:13 +0100

    ✨ Add Basic auth type

--- Sources/Netswift/Core/NetswiftAuthorizationType.swift
+++ Sources/Netswift/Core/NetswiftAuthorizationType.swift
@@ -10,15 +10,21 @@ import Foundation
 /**
  An Authorization HTTP Request header field.
  */
-public enum NetswiftAuthorizationType {
+public enum NetswiftAuthorizationType: Hashable {
     
     /// Bearer \<token\>
     case bearer(token: String)
+    
+    /// Basic \<token\>
+    case basic(token: String)
     
     var rawValue: String {
         switch self {
         case .bearer(let token):
             return "Bearer \(token)"
+            
+        case let .basic(token):
+            return "Basic \(token)"
         }
     }
 }
